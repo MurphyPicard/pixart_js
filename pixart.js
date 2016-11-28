b88647d303c42b2d9e177d59cffa06e63127bbe4
murphypicard <ayaghsizian@gmail.com>
2016-11-27 19:33:54 -0500

    20 divs created

--- pixart.js
+++ pixart.js
@@ -2,8 +2,21 @@ let $setColorButton = $(setColor);
 let $inputColorField = $(colorField);
 let $brushBox = $('.brush');
 
+//commit 1
 $setColorButton.on("click", function(){
   $brushBox.css("background-color", $inputColorField.val());
   // event.preventDefault();
   return false;
 });
+
+//commit 2
+$setColorButton.on("enter", function(){
+  $brushBox.css("background-color", $inputColorField.val());
+  // event.preventDefault();
+  return false;
+});
+
+//commit 3
+for (let i = 0; i<20; i++){
+$("body").append('<div class="square"></div>');
+}

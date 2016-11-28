bf998a806ff2c34ce188c701e7c5823f872bdf08
murphypicard <ayaghsizian@gmail.com>
2016-11-27 19:04:39 -0500

    initial commit input works

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+let $setColorButton = $(setColor);
+let $inputColorField = $(colorField);
+let $brushBox = $('.brush');
 
+$setColorButton.on("click", function(){
+  $brushBox.css("background-color", $inputColorField.val());
+  // event.preventDefault();
+  return false;
+});
